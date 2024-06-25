997c13e9bc5b173714bbffb7e2737aa684a0d232
AzarAhmadov <ehmedovh111@gmail.com>
2024-06-25 23:33:52 +0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import Header from '@/components/Header/Header'
+import Hero from '@/components/Hero/Hero'
 import React from 'react'
 
 const page = () => {
   return (
     <>
       <Header />
+      <main>
+        <Hero />
+      </main>
     </>
   )
 }
