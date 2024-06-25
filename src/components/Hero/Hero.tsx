6429a8f35da23acaa1fb88024057c622f1181eb9
AzarAhmadov <ehmedovh111@gmail.com>
2024-06-25 23:33:46 +0400

    Create Hero.tsx

---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hero = () => {
+    return (
+        <section>
+            <div className="container">
+                hero
+            </div>
+        </section>
+    )
+}
+
+export default Hero
