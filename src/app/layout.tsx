cf820a118ce957ea28498ee1b5a7734b3cfbcfe8
AzarAhmadov <ehmedovh111@gmail.com>
2024-06-25 23:22:50 +0400

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import '../css/style.css'
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -5,7 +7,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+      </body>
     </html>
   );
 }
