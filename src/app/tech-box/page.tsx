d2b1754e83365db668699335857b5b5eb594664b
AzarAhmadov <ehmedovh111@gmail.com>
2024-06-26 21:48:41 +0400

    Create page.tsx

---
+++ src/app/tech-box/page.tsx
@@ -0,0 +1,22 @@
+import Title from '@/common/Title/Title'
+import StackCards from '@/components/Stack/StackCards'
+import { StackData } from '@/data/data'
+import React from 'react'
+import { IoCodeSlashSharp } from 'react-icons/io5'
+
+const page = () => {
+    return (
+        <div className='container px-3 pb-10'>
+            <Title title={'My Tech Box'} icon={<IoCodeSlashSharp className='text-[30px]' />} />
+            <ul className='grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 gap-3 md:gap-5'>
+                {
+                    StackData.map((item, idx) => (
+                        <StackCards item={item} key={idx} />
+                    ))
+                }
+            </ul>
+        </div>
+    )
+}
+
+export default page
