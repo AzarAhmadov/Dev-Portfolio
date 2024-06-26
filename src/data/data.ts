b3a8c30386de71c2ba09048680ab4df995c199af
AzarAhmadov <ehmedovh111@gmail.com>
2024-06-26 13:07:10 +0400

    Create StackData

---
+++ src/data/data.ts
@@ -0,0 +1,42 @@
+export const StackData = [
+    {
+        name: "Html 5",
+        logo: "https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582748_1280.png",
+        link: "https://html.com/"
+    },
+    {
+        name: "Css 3",
+        logo: "https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582747_640.png",
+        link: "https://en.wikipedia.org/wiki/CSS"
+    },
+    {
+        name: "Sass",
+        logo: "https://w7.pngwing.com/pngs/437/300/png-transparent-sass-npm-cascading-style-sheets-node-js-sass-purple-violet-text-thumbnail.png",
+        link: "https://sass-lang.com/"
+    },
+    {
+        name: "JavaScript",
+        logo: "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png",
+        link: "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png"
+    },
+    {
+        name: "TypeScript",
+        logo: "https://w7.pngwing.com/pngs/915/519/png-transparent-typescript-hd-logo-thumbnail.png",
+        link: "https://www.typescriptlang.org/"
+    },
+    {
+        name: "Bootstrap",
+        logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqf-Kqyd8dSvhhufDguf9CsTZStGVsoSQ5dg&s",
+        link: "https://getbootstrap.com/"
+    },
+    {
+        name: "React",
+        logo: "https://cdn1.iconfinder.com/data/icons/programing-development-8/24/react_logo-512.png",
+        link: "https://react.dev/"
+    },
+    {
+        name: "Next.js",
+        logo: "https://www.svgrepo.com/show/354113/nextjs-icon.svg",
+        link: "https://nextjs.org/"
+    },
+]
