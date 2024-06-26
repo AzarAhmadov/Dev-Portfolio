f8a6e175ab04f6169c7f997aeea04a2e547eb419
AzarAhmadov <ehmedovh111@gmail.com>
2024-06-26 21:48:46 +0400

    Update Stack.tsx

--- src/components/Stack/Stack.tsx
+++ src/components/Stack/Stack.tsx
@@ -6,7 +6,7 @@ import StackCards from './StackCards'
 const Stack = () => {
     return (
         <section className='pt-[55px] md:pt-[100px] pb-10'>
-            <Heading title='Stack' href='' link='Full Stack' />
+            <Heading title='Stack' href='tech-box' link='Full Stack' />
             <ul className='grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 gap-3 md:gap-5 mt-7 md:mt-10'>
                 {
                     StackData.slice(0, 8).map((item, idx) => (
