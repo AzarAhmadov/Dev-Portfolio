1ed62eaa4c7f333c7359e6f80606395afb23b5de
AzarAhmadov <ehmedovh111@gmail.com>
2024-06-26 17:00:57 +0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Header from '@/components/Header/Header'
 import Hero from '@/components/Hero/Hero'
+import Projects from '@/components/Projects/Projects'
 import Stack from '@/components/Stack/Stack'
 import React from 'react'
 
@@ -10,6 +11,7 @@ const page = () => {
       <main>
         <Hero />
         <Stack />
+        <Projects />
       </main>
     </div>
   )
