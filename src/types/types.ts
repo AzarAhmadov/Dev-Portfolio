36fd2c08cb43371cdf5803f5dabd64476b4d11b3
AzarAhmadov <ehmedovh111@gmail.com>
2024-06-26 17:01:25 +0400

    Update types.ts

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,5 +1,31 @@
+// Heading Type
 export type HeadingType = {
     title: string
     link: string
     href: string
 }
+
+// Stack Type
+type Stack = {
+    name: string
+    link: string
+    logo: string
+}
+
+export type StackItem = {
+    item: Stack
+}
+
+// Project Type
+type Project = {
+    name: string;
+    link: string;
+    img: string;
+    lang: {
+        img: string;
+    }[];
+}
+
+export type ProjectsDataType = {
+    item: Project;
+};
