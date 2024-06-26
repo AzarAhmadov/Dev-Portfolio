88371744696321130b454c2ed5fcc8d766bc7be8
AzarAhmadov <ehmedovh111@gmail.com>
2024-06-26 17:01:33 +0400

    Add ProjectsData

--- src/data/data.ts
+++ src/data/data.ts
@@ -40,3 +40,88 @@ export const StackData = [
         link: "https://nextjs.org/"
     },
 ]
+
+export const ProjectsData = [
+    {
+        name: "Park Evenets",
+        link: "https://parkevents.vercel.app/",
+        img: "/p-1.png",
+        lang: [
+            {
+                img: "https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png",
+            },
+            {
+                img: "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/redux-icon.png",
+            },
+            {
+                img: "https://epss.ucla.edu/static/bootstrap-5.2.0/site/static/docs/5.2/assets/brand/bootstrap-social-logo.png"
+            },
+            {
+                img: "https://miro.medium.com/v2/resize:fit:1400/1*OgfGgWfcV4JEKUvNxf2AuA.png"
+            },
+            {
+                img: "https://sass-lang.com/assets/img/styleguide/seal-color.png",
+            },
+        ]
+    },
+    {
+        name: "Mustafa Kamal Ataturk",
+        link: "https://mustafakemalataturk.vercel.app/",
+        img: "/p-2.png",
+        lang: [
+            {
+                img: "https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png",
+            },
+            {
+                img: "https://cloud.egbontech.com/uploads/posts/1716756302.images__8_.png",
+            },
+            {
+                img: "https://sass-lang.com/assets/img/styleguide/seal-color.png",
+            },
+            {
+                img: "https://miro.medium.com/v2/resize:fit:1400/1*OgfGgWfcV4JEKUvNxf2AuA.png"
+            },
+            {
+                img: "https://avatars.githubusercontent.com/u/8546082?s=280&v=4"
+            },
+        ]
+    },
+    {
+        name: "Job Hub Center",
+        link: "https://jobhubcenter.com/en/",
+        img: "/p-3.png",
+        lang: [
+            {
+                img: "https://static-00.iconduck.com/assets.00/nextjs-icon-512x512-wbsw4aug.png",
+            },
+            {
+                img: "https://cloud.egbontech.com/uploads/posts/1716756302.images__8_.png",
+            },
+            {
+                img: "https://avatars.githubusercontent.com/u/8546082?s=280&v=4"
+            },
+            {
+                img: "https://i.pinimg.com/474x/91/23/7f/91237f1eca767ef74d722ca534f9281a.jpg"
+            },
+            {
+                img: "https://sass-lang.com/assets/img/styleguide/seal-color.png",
+            },
+        ]
+    },
+    {
+        name: "Watch Store",
+        link: "https://react-watch-store.vercel.app/",
+        img: "/p-4.png",
+        lang: [
+            {
+                img: "https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png",
+            },
+            {
+                img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOh9MeoM2tSOTjjG3Yyn6zxGA_sBlgfDw6-Q&s",
+            },
+            {
+                img: "https://sass-lang.com/assets/img/styleguide/seal-color.png",
+            },
+        ]
+    },
+]
