e3ca2b159a172242fe03e794cdd2149376b97662
AzarAhmadov <ehmedovh111@gmail.com>
2024-06-26 21:48:31 +0400

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Footer from '@/components/Footer/Footer';
 import '../css/style.css'
 
 export default function RootLayout({
@@ -9,6 +10,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         {children}
+        <Footer />
       </body>
     </html>
   );
