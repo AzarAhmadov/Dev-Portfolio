05b16774ec3699cc72eab43be33da81003a903de
AzarAhmadov <ehmedovh111@gmail.com>
2024-06-27 17:42:01 +0400

    Update ProjectsCards.tsx

--- src/components/Projects/ProjectsCards.tsx
+++ src/components/Projects/ProjectsCards.tsx
@@ -22,12 +22,12 @@ const ProjectsCard: FC<ProjectsDataType> = ({ item }) => {
                     <h4 className='font-medium md:text-[21px] text-[18px] text-black'>
                         {item.name}
                     </h4>
-                    <Link target='_blank' className='text-[16px] flex items-center gap-2 px-3 py-2 border-[1px] shadow-md border-[#00000023] rounded-full hover:bg-black hover:text-white transition-all' href={`${item.link}`}> View <IoMdArrowForward className='text-[20px]' /> </Link>
+                    <Link target='_blank' className='md:text-[16px] text-[15px] flex items-center md:gap-2 px-3 md:py-2 gap-1 py-1 border-[1px] shadow-md border-[#00000023] rounded-full hover:bg-black hover:text-white transition-all' href={`${item.link}`}> View <IoMdArrowForward className=' text-[18px] md:text-[20px]' /> </Link>
                 </div>
             </div>
             <div className='flex items-center gap-3 border-t-[1px] py-4 px-4 border-t-[#0000001d]'>
                 {item.lang.map((langItem, idx) => (
-                    <img key={idx} className='md:w-10 md:h-10 w-8 h-8 object-cover rounded-full' src={langItem.img} alt="" />
+                    <img key={idx} className='md:w-10 md:h-10 w-8 h-8 object-cover rounded-full' src={langItem.img} />
                 ))}
             </div>
         </div >
