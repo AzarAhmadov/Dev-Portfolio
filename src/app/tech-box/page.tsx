4be9877f09d22d8b842069587d6176c5f78bd760
AzarAhmadov <ehmedovh111@gmail.com>
2024-06-27 13:21:42 +0400

    Update page.tsx

--- src/app/tech-box/page.tsx
+++ src/app/tech-box/page.tsx
@@ -6,16 +6,18 @@ import { IoCodeSlashSharp } from 'react-icons/io5'
 
 const page = () => {
     return (
-        <div className='container px-3 pb-10'>
+        <>
             <Title title={'My Tech Box'} icon={<IoCodeSlashSharp className='text-[30px]' />} />
-            <ul className='grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 gap-3 md:gap-5'>
-                {
-                    StackData.map((item, idx) => (
-                        <StackCards item={item} key={idx} />
-                    ))
-                }
-            </ul>
-        </div>
+            <div className='container px-3 pb-10 pt-4'>
+                <ul className='grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 gap-3 md:gap-5'>
+                    {
+                        StackData.map((item, idx) => (
+                            <StackCards item={item} key={idx} />
+                        ))
+                    }
+                </ul>
+            </div>
+        </>
     )
 }
 
