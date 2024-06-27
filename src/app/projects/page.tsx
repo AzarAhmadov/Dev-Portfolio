9d90a119feb2eab9462536dda560a5d80e28f8d2
AzarAhmadov <ehmedovh111@gmail.com>
2024-06-27 13:21:19 +0400

    Update page.tsx

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,9 +1,22 @@
+import Title from '@/common/Title/Title'
+import ProjectsCard from '@/components/Projects/ProjectsCards';
+import { ProjectsData } from '@/data/data';
 import React from 'react'
+import { PiProjectorScreenBold } from "react-icons/pi";
 
 const page = () => {
     return (
         <>
-            page
+            <Title title={'All Projects'} icon={<PiProjectorScreenBold className='text-[30px]' />} />
+            <div className='container px-3 pb-10 pt-4'>
+                <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
+                    {
+                        ProjectsData.map((item, idx) => (
+                            <ProjectsCard item={item} key={idx} />
+                        ))
+                    }
+                </div>
+            </div>
         </>
     )
 }
