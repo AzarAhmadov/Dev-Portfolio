45d0b1fa07c4ac758c28af29fa1a4f55d8434a75
AzarAhmadov <ehmedovh111@gmail.com>
2024-06-28 13:20:02 +0400

    Update Title.tsx

--- src/common/Title/Title.tsx
+++ src/common/Title/Title.tsx
@@ -8,7 +8,7 @@ const Title: FC<TitleType> = ({ title, icon }) => {
         <div className='border-b-[1px] border-b-[#00000011] px-4'>
             <div className='flex items-center justify-between py-4 md:py-5 container'>
                 <Link className='border-[1px] border-[#000] rounded-full px-3 py-1 hover:bg-black transition-all hover:text-white' href={'/'}> <GoArrowLeft className='text-[23px]' /> </Link>
-                <h3 className='justify-self-center font-medium text-[20px] md:text-[23px]'>
+                <h3 className='justify-self-center font-medium text-[20px] md:text-[27px]'>
                     {title}
                 </h3>
                 {icon}
