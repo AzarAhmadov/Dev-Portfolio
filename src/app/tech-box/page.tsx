f777375e71b0e220d97d2882ca1964316e0496fa
AzarAhmadov <ehmedovh111@gmail.com>
2024-06-28 13:20:07 +0400

    Update page.tsx

--- src/app/tech-box/page.tsx
+++ src/app/tech-box/page.tsx
@@ -14,7 +14,7 @@ const page = () => {
     return (
         <>
             <Title title={'My Tech Box'} icon={<IoCodeSlashSharp className='text-[25px]' />} />
-            <div className='container px-3 py-5 md:py-7'>
+            <main className='container px-3 py-5 md:py-7'>
                 <ul className='grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 gap-3 md:gap-5'>
                     {
                         StackData.map((item, idx) => (
@@ -22,7 +22,7 @@ const page = () => {
                         ))
                     }
                 </ul>
-            </div>
+            </main>
         </>
     )
 }
