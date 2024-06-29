e9d70e8afef35519a72815416cf236851d72541d
AzarAhmadov <ehmedovh111@gmail.com>
2024-06-29 11:36:22 +0400

    Update Hero.tsx

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -19,20 +19,26 @@ const Hero = () => {
                     <h2 className='color-black text-[17px] md:text-[25px] font-normal mt-1 mb-4'>
                         Front-End Developer
                     </h2>
-                    <p className='text-[14px] leading-[20px] md:text-[17px] font-thin  md:leading-[26px] text-gray'>
+                    <p className='text-[14px] leading-[20px] md:text-[17px] font-thin md:leading-[26px] text-gray'>
                         I currently reside in Baku. For about 4 years, I have been actively trying to improve my skills in the field of technology and constantly applying what I have learned. I had the opportunity to work in various technology companies, and I am pleased about that.
                     </p>
                 </div>
             </div>
-            <div className='container flex-wrap flex justify-center gap-5 md:gap-0 md:justify-between items-center pt-6 md:pt-[70px]'>
-                <span className='flex items-center gap-2 text-[17px]'>
-                    <BiMap className='text-[20px]' />  Baku, Azerbaijan
-                </span>
-                <Link className=' md:text-[17px] text-[15px] flex items-center gap-2 md:px-5 md:py-3 py-2 px-4 border-[1px] shadow-sm border-[#00000023] rounded-full hover:bg-black hover:text-white transition-all' href={'/about'}> More about Me <IoMdArrowForward className='text-[20px]' /> </Link>
-                <a href="#" className='bg-black text-white hover:bg-transparent hover:border-[1px] hover:
+            <div className='container flex flex-wrap justify-center md:justify-between gap-5 gap-x-8 md:gap-x-0 md:gap-0 pt-6 md:pt-[70px]'>
+                <div className='flex md:w-[33.3%]'>
+                    <span className='flex items-center gap-2 text-[17px]'>
+                        <BiMap className='text-[20px]' />  Baku, Azerbaijan
+                    </span>
+                </div>
+                <div className='flex justify-center md:w-[33.3%]'>
+                    <a href="#" className='bg-black text-white hover:bg-transparent hover:border-[1px] hover:
                 border-[#00000071] hover:text-black transition-all md:text-[17px] text-[15px] hover:scale-90 gap-2 rounded-full md:px-5 md:py-3 py-2 px-4 flex items-center'>
-                    My Cv <MdOutlineFileDownload className='text-[22px]' />
-                </a>
+                        My Cv <MdOutlineFileDownload className='text-[22px]' />
+                    </a>
+                </div>
+                <div className='flex md:justify-end justify-center md:w-[33.3%] w-[100%]'>
+                    <Link className=' md:text-[17px] text-[15px] flex items-center gap-2 md:px-5 md:py-3 py-2 px-4 border-[1px] shadow-sm border-[#00000023] rounded-full hover:bg-black hover:text-white transition-all' href={'/about'}> More about Me <IoMdArrowForward className='text-[20px]' /> </Link>
+                </div>
             </div>
         </section>
     )
