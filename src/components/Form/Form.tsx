cdbbcb887c8a0cdcf3946a2443d40f05b91767bc
AzarAhmadov <ehmedovh111@gmail.com>
2024-06-29 15:15:26 +0400

    Update Form.tsx

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,48 +1,124 @@
+
+import emailjs from "@emailjs/browser";
 import { ModalType } from '@/types/types';
-import React, { FC } from 'react'
+import React, { FC, useRef } from 'react'
 import { IoCloseSharp } from "react-icons/io5";
 import { TbSend } from "react-icons/tb";
+import { ToastContainer, toast } from 'react-toastify';
 
 const Form: FC<ModalType> = ({ modal, toggle }) => {
+
+    const form = useRef<HTMLFormElement>(null);
+    const name = useRef<HTMLInputElement>(null);
+    const email = useRef<HTMLInputElement>(null);
+    const message = useRef<HTMLTextAreaElement>(null);
+
+    const sendEmail = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        if (process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID && process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID && process.env.NEXT_PUBLIC_EMAILJS_USER_ID && form.current) {
+            emailjs
+                .sendForm(
+                    process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID,
+                    process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID,
+                    form.current,
+                    process.env.NEXT_PUBLIC_EMAILJS_USER_ID
+                )
+                .then(
+                    () => {
+                        toast.success('Message sent successfully!', {
+                            position: "top-right",
+                            autoClose: 5000,
+                            hideProgressBar: false,
+                            closeOnClick: true,
+                            pauseOnHover: true,
+                            draggable: true,
+                            theme: "light",
+                        })
+                    },
+                    () => {
+                        toast.error('Message could not be sent!', {
+                            position: "top-right",
+                            autoClose: 5000,
+                            hideProgressBar: false,
+                            closeOnClick: true,
+                            pauseOnHover: true,
+                            draggable: true,
+                            theme: "light",
+                        });
+                    }
+                );
+        }
+
+        toggle()
+
+        if (name.current) name.current.value = "";
+        if (email.current) email.current.value = "";
+        if (message.current) message.current.value = "";
+
+    };
+
     return (
-        <div className={`fixed inset-0 px-4 flex items-center justify-center bg-black bg-opacity-50 backdrop-blur-sm transition-all duration-400 ${modal ? 'opacity-100 visible' : 'opacity-0 invisible'}`}>
-            <div className={`bg-white rounded-[15px] shadow-md w-full max-w-[400px] transition-all ${modal ? 'scale-100' : 'scale-75'}`}>
-                <div className='flex items-center justify-between px-6 py-5 border-b-[1px] border-b-[#00000027]'>
-                    <h3 className="text-[21px] font-medium text-center">Contact Me</h3>
-                    <IoCloseSharp onClick={toggle} className='text-[30px] hover:text-[red] transition-all cursor-pointer' />
+        <>
+            <div className={`fixed inset-0 px-4 flex items-center justify-center bg-black bg-opacity-50 backdrop-blur-sm transition-all duration-400 ${modal ? 'opacity-100 visible' : 'opacity-0 invisible'}`}>
+                <div className={`bg-white rounded-[15px] shadow-md w-full max-w-[400px] transition-all ${modal ? 'scale-100' : 'scale-75'}`}>
+                    <div className='flex items-center justify-between px-6 py-5 border-b-[1px] border-b-[#00000027]'>
+                        <h3 className="text-[21px] font-medium text-center">Contact Me</h3>
+                        <IoCloseSharp onClick={toggle} className='text-[30px] hover:text-[red] transition-all cursor-pointer' />
+                    </div>
+                    <form onSubmit={sendEmail} ref={form} className='p-6'>
+                        <div className="mb-4">
+                            <input
+                                type="text"
+                                required
+                                ref={name}
+                                name="name"
+                                placeholder="Name"
+                                className="w-full bg-[#f4f5f534] text-[15px] placeholder:text-[15px] p-3 border border-[#00000036] placeholder:text-black rounded-full bg-gray-100 outline-none"
+                            />
+                        </div>
+                        <div className="mb-4">
+                            <input
+                                type="email"
+                                required
+                                ref={email}
+                                name="email"
+                                placeholder="Email"
+                                className="w-full bg-[#f4f5f534] text-[15px] placeholder:text-[15px] p-3 border border-[#00000036] rounded-full bg-gray-100 outline-none placeholder:text-black"
+                            />
+                        </div>
+                        <div className="mb-4">
+                            <textarea
+                                required
+                                ref={message}
+                                name="message"
+                                placeholder="Message"
+                                className="w-full bg-[#f4f5f534] text-[15px] placeholder:text-[15px] h-[150px] p-3 border border-[#00000036] rounded-3xl bg-gray-100 outline-none resize-none placeholder:text-black"
+                            ></textarea>
+                        </div>
+                        <div className="flex justify-center">
+                            <button className="px-6 flex items-center gap-2 text-[16px] hover:scale-110 transition-all py-3 bg-black text-white rounded-full hover:bg-gray-800">
+                                Send Message <TbSend className='text-[#fff] text-[18px]' />
+                            </button>
+                        </div>
+                    </form>
                 </div>
-                <form className='p-6' action="">
-                    <div className="mb-4">
-                        <input
-                            type="text"
-                            required
-                            placeholder="Name"
-                            className="w-full bg-[#f4f5f534] text-[15px] placeholder:text-[15px] p-3 border border-[#00000036] placeholder:text-black rounded-full bg-gray-100 outline-none"
-                        />
-                    </div>
-                    <div className="mb-4">
-                        <input
-                            type="email"
-                            required
-                            placeholder="Email"
-                            className="w-full bg-[#f4f5f534] text-[15px] placeholder:text-[15px] p-3 border border-[#00000036] rounded-full bg-gray-100 outline-none placeholder:text-black"
-                        />
-                    </div>
-                    <div className="mb-4">
-                        <textarea
-                            required
-                            placeholder="Message"
-                            className="w-full bg-[#f4f5f534] text-[15px] placeholder:text-[15px] h-[150px] p-3 border border-[#00000036] rounded-3xl bg-gray-100 outline-none resize-none placeholder:text-black"
-                        ></textarea>
-                    </div>
-                    <div className="flex justify-center">
-                        <button className="px-6 flex items-center gap-2 text-[16px] hover:scale-110 transition-all py-3 bg-black text-white rounded-full hover:bg-gray-800">
-                            Send Message <TbSend className='text-[#fff] text-[18px]' />
-                        </button>
-                    </div>
-                </form>
             </div>
-        </div>
+
+            <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+            />
+            <ToastContainer />
+        </>
     )
 }
 
