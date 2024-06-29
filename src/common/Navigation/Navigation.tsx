795ed25ac416f8342f03ef03e13b959046993f8c
AzarAhmadov <ehmedovh111@gmail.com>
2024-06-29 11:36:15 +0400

    Create Navigation.tsx

---
+++ src/common/Navigation/Navigation.tsx
@@ -0,0 +1,48 @@
+'use client'
+
+import React from 'react'
+import Link from 'next/link'
+import { IoHomeOutline } from "react-icons/io5";
+import { FaCode } from "react-icons/fa6";
+import { GrPersonalComputer } from "react-icons/gr";
+import { LuUser2 } from "react-icons/lu";
+import { usePathname } from 'next/navigation';
+
+
+const Navigation = () => {
+
+    const pathname = usePathname()
+
+    return (
+        <section className='fixed left-[50%] translate-x-[-50%] bottom-10'>
+            <nav className='flex justify-center gap-3 bg-[#FBFBFC]/70 backdrop-blur-lg md:px-3 py-2 px-2 border-[1px] border-[#00000014] rounded-full'>
+                <Link
+                    href={'/'}
+                    className={`${pathname === '/' ? 'bg-[#000] text-white' : 'bg-[#fff]'} transition-all shadow-md rounded-full w-[50px] h-[50px] flex items-center justify-center shrink-0`}
+                >
+                    <IoHomeOutline className={`${pathname === '/' ? 'scale-90' : 'scale-100'}  text-[25px]`} />
+                </Link>
+                <Link
+                    href={'/about'}
+                    className={`${pathname === '/about' ? 'bg-[#000] text-white' : 'bg-[#fff]'} transition-all shadow-md rounded-full w-[50px] h-[50px] flex items-center justify-center shrink-0`}
+                >
+                    <LuUser2 className={`${pathname === '/about' ? 'scale-90' : 'scale-100'} text-[25px]`} />
+                </Link>
+                <Link
+                    href={'/tech-box'}
+                    className={`${pathname === '/tech-box' ? 'bg-[#000] text-white' : 'bg-[#fff]'} transition-all shadow-md rounded-full w-[50px] h-[50px] flex items-center justify-center shrink-0`}
+                >
+                    <FaCode className={`${pathname === '/tech-box' ? 'scale-90' : 'scale-100'} text-[25px]`} />
+                </Link>
+                <Link
+                    href={'/projects'}
+                    className={`${pathname === '/projects' ? 'bg-[#000] text-white' : 'bg-[#fff]'} transition-all shadow-md rounded-full w-[50px] h-[50px] flex items-center justify-center shrink-0`}
+                >
+                    <GrPersonalComputer className={`${pathname === '/projects' ? 'scale-90' : 'scale-100'} text-[25px]`} />
+                </Link>
+            </nav>
+        </section>
+    )
+}
+
+export default Navigation
