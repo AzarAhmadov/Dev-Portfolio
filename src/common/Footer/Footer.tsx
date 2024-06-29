a4a52d30b148eacac47d97bc85e0d10fb5815fcf
AzarAhmadov <ehmedovh111@gmail.com>
2024-06-29 16:00:20 +0400

    Update Footer.tsx

--- src/common/Footer/Footer.tsx
+++ src/common/Footer/Footer.tsx
@@ -16,7 +16,7 @@ const Footer = () => {
 
     return (
         <>
-            <footer className='flex flex-col items-center justify-center pt-7 pb-[140px] border-t-[1px] border-t-[#00000013]'>
+            <footer className='flex flex-col items-center justify-center pt-7 pb-[130px] border-t-[1px] border-t-[#00000013]'>
                 <h4 className='text-center text-black text-[23px]'>
                     Have a <br />
                     Project Idea?
