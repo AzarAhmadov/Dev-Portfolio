6b80029c57fd95c90b7081cbde1be72be70e061a
AzarAhmadov <ehmedovh111@gmail.com>
2024-06-30 20:59:58 +0400

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import Footer from '@/common/Footer/Footer';
 import Navigation from '@/common/Navigation/Navigation';
-import Head from 'next/head';
 import '@/css/style.css'
 
 const metadata = {
@@ -22,20 +21,18 @@ export default function RootLayout({
 }>) {
   return (
     <html>
-      <Head>
-        <link rel="icon" href="https://cdn1.iconfinder.com/data/icons/round-icons-vol-2/512/Code_javascript_development-512.png" />
-        <meta name="keywords" content="Azar Ahmadov, Azər Əhmədov, Web Developer, Front-end developer, Html, Css, Js, Javascript, React, React.js, Next js, Node, Node.js" />
-        <meta name="robots" content="index, follow" />
-        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
-        <meta name="language" content="English" />
-        <meta name="author" content="Azar Ahmadov" />
-        <meta property="og:title" content={metadata.title.default} />
-        <meta property="og:description" content={metadata.description} />
-        <meta property="og:url" content={metadata.url} />
-        <meta property="og:image" content={metadata.image} />
-        <meta property="og:type" content={metadata.type} />
-        <meta property="og:locale" content={metadata.locale} />
-      </Head>
+      <link rel="icon" type="image/x-icon" href='/logo.png' />
+      <meta name="keywords" content="Azar Ahmadov, Azər Əhmədov, Web Developer, Front-end developer, Html, Css, Js, Javascript, React, React.js, Next js, Node, Node.js" />
+      <meta name="robots" content="index, follow" />
+      <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+      <meta name="language" content="English" />
+      <meta name="author" content="Azar Ahmadov" />
+      <meta property="og:title" content={metadata.title.default} />
+      <meta property="og:description" content={metadata.description} />
+      <meta property="og:url" content={metadata.url} />
+      <meta property="og:image" content={metadata.image} />
+      <meta property="og:type" content={metadata.type} />
+      <meta property="og:locale" content={metadata.locale} />
 
       <body className='dark:bg-[#0a0d19]'>
         {children}
