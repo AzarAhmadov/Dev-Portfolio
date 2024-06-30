36ad552b87d980134f5d49b9b5d4c747fbae397a
AzarAhmadov <ehmedovh111@gmail.com>
2024-06-30 14:34:22 +0400

    Update

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -3,14 +3,14 @@
 import { motion } from "framer-motion";
 
 const variants = {
-    hidden: { opacity: 0, x: -20 },
+    hidden: { opacity: 0, x: -15 },
     enter: {
         opacity: 1,
         x: 0,
         transition: {
             type: "spring",
-            stiffness: 100,
-            damping: 20,
+            stiffness: 90,
+            damping: 30,
         },
     },
 };
