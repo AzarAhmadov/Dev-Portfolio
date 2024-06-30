7983de276f2cfac6be81275c61cae87f05d3c80c
AzarAhmadov <ehmedovh111@gmail.com>
2024-06-30 20:53:04 +0400

    add The Open Graph protocol

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,19 @@
 import Footer from '@/common/Footer/Footer';
-import '../css/style.css'
-import { Metadata } from 'next';
 import Navigation from '@/common/Navigation/Navigation';
+import Head from 'next/head';
+import '@/css/style.css'
 
-export const metadata: Metadata = {
+const metadata = {
   title: {
     default: "Azar Ahmadov | Personal Portfolio",
     template: `Azar Ahmadov | %s `,
   },
-  description: 'Azar Ahmadov personal website. This is a place where you can find my projects and information about me.',
-  metadataBase: new URL('https://azarahmadov.com'),
-  alternates: {
-    canonical: '/',
-    languages: {
-      'en-US': '/en-US',
-    },
-  }
-}
+  description: "Azar Ahmadov personal website. This is a place where you can find my projects and information about me.",
+  url: "https://azarahmadov.com/",
+  image: "/hero.jpg",
+  type: "website",
+  locale: "en_US"
+};
 
 export default function RootLayout({
   children,
@@ -24,15 +21,22 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <head>
+    <html>
+      <Head>
         <link rel="icon" href="https://cdn1.iconfinder.com/data/icons/round-icons-vol-2/512/Code_javascript_development-512.png" />
         <meta name="keywords" content="Azar Ahmadov, Azər Əhmədov, Web Developer, Front-end developer, Html, Css, Js, Javascript, React, React.js, Next js, Node, Node.js" />
         <meta name="robots" content="index, follow" />
         <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
         <meta name="language" content="English" />
         <meta name="author" content="Azar Ahmadov" />
-      </head>
+        <meta property="og:title" content={metadata.title.default} />
+        <meta property="og:description" content={metadata.description} />
+        <meta property="og:url" content={metadata.url} />
+        <meta property="og:image" content={metadata.image} />
+        <meta property="og:type" content={metadata.type} />
+        <meta property="og:locale" content={metadata.locale} />
+      </Head>
+
       <body className='dark:bg-[#0a0d19]'>
         {children}
         <Navigation />
