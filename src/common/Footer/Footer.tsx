b63af7e2776cbb0b8bf3d8a051e4e3d48b3d9b04
AzarAhmadov <ehmedovh111@gmail.com>
2024-07-23 20:36:59 +0400

    Update Footer.tsx

--- src/common/Footer/Footer.tsx
+++ src/common/Footer/Footer.tsx
@@ -17,14 +17,17 @@ const Footer = () => {
     return (
         <>
             <footer className='flex flex-col items-center justify-center pt-7 pb-[130px] border-t-[1px] border-t-[#00000013] dark:border-t-[#ffffff2b]'>
+
                 <h4 className='text-center text-black dark:text-white text-[23px]'>
-                    Have a <br />
-                    Project Idea?
+                    Do you want to <br /> contact me?
                 </h4>
-                <button disabled={modal} onClick={toggle} className='my-4 flex items-center gap-2 dark:bg-white dark:hover:text-black dark:hover:bg-[#ffffffe7] md:px-5 px-4 py-2 w-[max-content] md:py-3 border-[1px] shadow-md border-[#00000023] rounded-full hover:bg-black hover:text-white transition-all'> Let's Connect <IoMdArrowForward className='text-[20px]' /> </button>
+
+                <button disabled={modal} onClick={toggle} className='my-4 flex items-center gap-2 dark:bg-white dark:hover:text-black dark:hover:bg-[#ffffffe7] md:px-5 px-4 py-2 w-[max-content] md:py-3 border-[1px] shadow-md border-[#00000023] rounded-full hover:bg-black hover:text-white transition-all'> Let's Contact <IoMdArrowForward className='text-[20px]' /> </button>
+
                 <Link aria-label="gmail" href={'mailto:azarahmadovdev@gmail.com'} className='text-[14px] dark:border-[#ffffffb6] dark:text-[#ffffffd3] mt-1 border-[1px] rounded-full px-3 py-1 text-gray border-[#00000044]'>
                     azarahmadovdev@gmail.com
                 </Link>
+
                 <ul className='flex gap-3 mb-6 mt-5'>
                     <li>
                         <Link target='_blank' href={'https://www.linkedin.com/in/azar-ahmadov/'}>
@@ -42,12 +45,15 @@ const Footer = () => {
                         </Link>
                     </li>
                 </ul>
-                <p className='text-gray dark:text-[#ffffffcf]'>
+
+                <p className='text-gray text-[14px] md:text-[16px] dark:text-[#ffffffcf]'>
                     © Copyright 2024. All rights Reserved.
                 </p>
-                <p className='mt-2 dark:text-white font-medium'>
+
+                <p className='mt-2 text-[13px] md:text-[15px] dark:text-white font-medium'>
                     <span className='text-gray dark:text-[#ffffffcf] font-normal'>Made by</span> Azar Ahmadov
                 </p>
+
             </footer>
 
             <Form modal={modal} toggle={toggle} />
