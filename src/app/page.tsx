a14b3a17c8f22d5e3b4ff62abdfb0c89824ccf02
AzarAhmadov <ehmedovh111@gmail.com>
2024-10-14 23:40:49 +0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,21 @@
-import React from 'react'
-import Header from '@/components/Header/Header'
-const Projects = dynamic(() => import('@/components/Projects/Projects'))
-const Stack = dynamic(() => import('@/components/Stack/Stack'))
-import Hero from '@/components/Hero/Hero'
-import dynamic from 'next/dynamic'
+import React from "react";
+import Header from "@/components/Header/Header";
+const Projects = dynamic(() => import("@/components/Projects/Projects"));
+const Stack = dynamic(() => import("@/components/Stack/Stack"));
+import Hero from "@/components/Hero/Hero";
+import dynamic from "next/dynamic";
 
 const page = () => {
   return (
-    <>
-      <div className='container px-4 md:px-5'>
-        <Header />
-        <main>
-          <Hero />
-          <Stack />
-          <Projects />
-        </main>
-      </div>
-    </>
-  )
-}
+    <div className="container px-4 md:px-5">
+      <Header />
+      <main>
+        <Hero />
+        <Stack />
+        <Projects />
+      </main>
+    </div>
+  );
+};
 
-export default page
+export default page;
