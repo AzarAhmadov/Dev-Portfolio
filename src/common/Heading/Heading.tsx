6d1050abdea4d3ff0a63cd2667be3ef0a22aecc9
AzarAhmadov <ehmedovh111@gmail.com>
2024-10-14 23:40:44 +0400

    Update Heading.tsx

--- src/common/Heading/Heading.tsx
+++ src/common/Heading/Heading.tsx
@@ -1,20 +1,23 @@
-
-import React, { FC, memo } from 'react'
-import { HeadingType } from '@/types/types'
-import Link from 'next/link'
-import { IoMdArrowForward } from 'react-icons/io'
+import React, { FC, memo } from "react";
+import { HeadingType } from "@/types/types";
+import Link from "next/link";
+import { IoMdArrowForward } from "react-icons/io";
 
 const Heading: FC<HeadingType> = ({ title, href, link }) => {
-    return (
-        <section>
-            <div className='flex items-center justify-between'>
-                <h4 className='text-[21px] dark:text-white md:text-[25px] font-medium'> {title} </h4>
-                <Link aria-label='Get Link' className='flex items-center gap-2 px-3 text-[15px] md:text-[17px] md:px-4 py-2 border-[1px] shadow-md dark:border-[#ffffffa4] border-[#00000023] hover:dark:bg-white dark:hover:text-black dark:text-white rounded-full hover:bg-black hover:text-white transition-all' href={`/${href}`}>
-                    {link} <IoMdArrowForward className='text-[18px]' />
-                </Link>
-            </div>
-        </section>
-    )
-}
+  return (
+    <section className="flex items-center justify-between">
+      <h4 className="text-[21px] text-black dark:text-white md:text-[25px] font-medium">
+        {title}
+      </h4>
+      <Link
+        aria-label="Get Link"
+        className="flex items-center gap-2 px-3 text-[15px] md:text-[17px] md:px-4 py-2 border-[1px] shadow-md dark:border-[#ffffffa4] border-[#00000023] hover:dark:bg-white dark:hover:text-black dark:text-white rounded-full hover:bg-black hover:text-white transition-all"
+        href={`/${href}`}
+      >
+        {link} <IoMdArrowForward className="text-[18px]" />
+      </Link>
+    </section>
+  );
+};
 
-export default memo(Heading)
+export default memo(Heading);
