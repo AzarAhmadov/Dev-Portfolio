5e1fc411abb6763d395c16fc68ac1ffdec418888
AzarAhmadov <ehmedovh111@gmail.com>
2024-10-14 23:40:55 +0400

    Update Navigation.tsx

--- src/common/Navigation/Navigation.tsx
+++ src/common/Navigation/Navigation.tsx
@@ -1,51 +1,41 @@
-'use client'
+"use client";
 
-import React from 'react'
-import Link from 'next/link'
-import { IoHomeOutline } from "react-icons/io5";
-import { FaCode } from "react-icons/fa6";
-import { GrPersonalComputer } from "react-icons/gr";
-import { LuUser2 } from "react-icons/lu";
-import { usePathname } from 'next/navigation';
+import React from "react";
+import { NavLinks } from "@/data/data";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Navigation = () => {
+  const pathName = usePathname();
 
-    const pathname = usePathname()
+  return (
+    <section className="fixed left-[50%] translate-x-[-50%] bottom-10">
+      <nav className="flex justify-center gap-3 bg-[#FBFBFC]/70 backdrop-blur-lg md:px-3 py-2 px-2 border-[1px] border-[#00000014] rounded-full">
+        {NavLinks.map((link, index) => {
+          const Icon = link.icon;
 
-    return (
-        <section className='fixed left-[50%] translate-x-[-50%] bottom-10'>
-            <nav className='flex justify-center gap-3 bg-[#FBFBFC]/70 backdrop-blur-lg md:px-3 py-2 px-2 border-[1px] border-[#00000014] rounded-full'>
-                <Link
-                    aria-label="Home"
-                    href={'/'}
-                    className={`${pathname === '/' ? 'bg-gradient-to-l from-zinc-900 to-stone-700 text-white' : 'bg-[#fff]'} transition-all shadow-md rounded-full w-[50px] h-[50px] flex items-center justify-center shrink-0`}
-                >
-                    <IoHomeOutline className={`${pathname === '/' ? 'scale-90' : 'scale-100'}  text-[25px]`} />
-                </Link>
-                <Link
-                    aria-label="About"
-                    href={'/about'}
-                    className={`${pathname === '/about' ? 'bg-gradient-to-l from-zinc-900 to-stone-700 text-white' : 'bg-[#fff]'} transition-all shadow-md rounded-full w-[50px] h-[50px] flex items-center justify-center shrink-0`}
-                >
-                    <LuUser2 className={`${pathname === '/about' ? 'scale-90' : 'scale-100'} text-[25px]`} />
-                </Link>
-                <Link
-                    aria-label="Tech Box"
-                    href={'/tech-box'}
-                    className={`${pathname === '/tech-box' ? 'bg-gradient-to-l from-zinc-900 to-stone-700 text-white' : 'bg-[#fff]'} transition-all shadow-md rounded-full w-[50px] h-[50px] flex items-center justify-center shrink-0`}
-                >
-                    <FaCode className={`${pathname === '/tech-box' ? 'scale-90' : 'scale-100'} text-[25px]`} />
-                </Link>
-                <Link
-                    aria-label="Projects"
-                    href={'/projects'}
-                    className={`${pathname === '/projects' ? 'bg-gradient-to-l from-zinc-900 to-stone-700 text-white' : 'bg-[#fff]'} transition-all shadow-md rounded-full w-[50px] h-[50px] flex items-center justify-center shrink-0`}
-                >
-                    <GrPersonalComputer className={`${pathname === '/projects' ? 'scale-90' : 'scale-100'} text-[25px]`} />
-                </Link>
-            </nav>
-        </section>
-    )
-}
+          return (
+            <Link
+              className={`
+                ${
+                  pathName === link.href
+                    ? "bg-gradient-to-l from-zinc-900 to-stone-700 text-white"
+                    : "bg-[#fff]"
+                }
+                transition-all shadow-md rounded-full w-[50px] h-[50px] flex items-center justify-center shrink-0
+                
+                `}
+              key={index}
+              href={link.href}
+              aria-label={link.ariaLabel}
+            >
+              <Icon className="text-[25px]" />
+            </Link>
+          );
+        })}
+      </nav>
+    </section>
+  );
+};
 
-export default Navigation
+export default Navigation;
