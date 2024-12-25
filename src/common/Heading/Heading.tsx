37628aac50541fafba88888238529b6dc5600165
AzarAhmadov <ehmedovh111@gmail.com>
2024-12-25 22:19:31 +0400

    Update Heading.tsx

--- src/common/Heading/Heading.tsx
+++ src/common/Heading/Heading.tsx
@@ -9,13 +9,15 @@ const Heading: FC<HeadingType> = ({ title, href, link }) => {
       <h4 className="text-[21px] text-black dark:text-white md:text-[25px] font-medium">
         {title}
       </h4>
-      <Link
-        aria-label="Get Link"
-        className="flex items-center gap-2 px-3 text-[15px] md:text-[17px] md:px-4 py-2 border-[1px] shadow-md dark:border-[#ffffffa4] border-[#00000023] hover:dark:bg-white dark:hover:text-black dark:text-white rounded-full hover:bg-black hover:text-white transition-all"
-        href={`/${href}`}
-      >
-        {link} <IoMdArrowForward className="text-[18px]" />
-      </Link>
+      {link && (
+        <Link
+          aria-label="Get Link"
+          className="flex items-center gap-2 px-3 text-[15px] md:text-[17px] md:px-4 py-2 border-[1px] shadow-md dark:border-[#ffffffa4] border-[#00000023] hover:dark:bg-white dark:hover:text-black dark:text-white rounded-full hover:bg-black hover:text-white transition-all"
+          href={`/${href}`}
+        >
+          {link} <IoMdArrowForward className="text-[18px]" />
+        </Link>
+      )}
     </section>
   );
 };
