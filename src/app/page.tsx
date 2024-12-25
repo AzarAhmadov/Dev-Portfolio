ac2e9280bae0b3cd64040232716f0c956c826a33
AzarAhmadov <ehmedovh111@gmail.com>
2024-12-25 22:19:19 +0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ const Projects = dynamic(() => import("@/components/Projects/Projects"));
 const Stack = dynamic(() => import("@/components/Stack/Stack"));
 import Hero from "@/components/Hero/Hero";
 import dynamic from "next/dynamic";
+import Heading from "@/common/Heading/Heading";
+import { YoutubeVideos } from "@/data/data";
 
 const page = () => {
   return (
@@ -11,6 +13,22 @@ const page = () => {
       <Header />
       <main>
         <Hero />
+        <div className="mt-20">
+          <Heading title="Latest YouTube Videos" />
+          <div className="grid grid-cols-2 md:grid-cols-4 gap-4 mt-7 md:mt-10">
+            {YoutubeVideos.map((el, idx) => (
+              <iframe
+                className=" aspect-[7/8] w-full bg-red-50 rounded-lg"
+                key={idx}
+                src={el.src}
+                title={el.title}
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                referrerpolicy="strict-origin-when-cross-origin"
+                allowfullscreen
+              />
+            ))}
+          </div>
+        </div>
         <Stack />
         <Projects />
       </main>
